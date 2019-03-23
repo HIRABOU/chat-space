3ad576ebef958a26dda4c4b5969414283ce20e6d
HIRABOU <owner@ownernoMacBook-puro.local>
2019-03-23 14:36:49 +0900

    add association
    add association code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  has_many :messages
+  has_many :members
+  has_many :groups, through: :members
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
