85bb403d920323c13f608cd537d1d3ac24e4f77a
HIRABOU <owner@ownernoMacBook-puro.local>
2019-03-24 13:28:52 +0900

    Gem install
    install mini_magick and Carrierwave

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,9 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  Include CarrierWave::MiniMagick
+  storage :file
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
+
+    process resize_to_fit: [800, 800]
+end
